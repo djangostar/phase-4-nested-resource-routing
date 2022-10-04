84cd0854ea4eac242491c370b30a885f49a3b36e
Juan-Sergio Khoshdel <jsergiofm85@gmail.com>
2022-10-03 23:27:31 -0700

    Done.

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,10 +3,8 @@ class ReviewsController < ApplicationController
 
   def index
     if params[:dog_house_id]
-      dog_house = Doghouse.find(params[:dog_house_id])
+      dog_house = DogHouse.find(params[:dog_house_id])
       reviews = dog_house.reviews
-    else
-      reviews = Review.all
     end
     render json: reviews, include: :dog_house
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
     resources :reviews, only: [:show, :index]
   end
   resources :reviews, only: [:show, :index, :create]
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
